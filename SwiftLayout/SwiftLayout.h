972ebe0534b08a4c36752de06ba789c8df546355
Nick Rudden <nrudden@magicseaweed.com>
2019-11-30 13:08:51 +0000

    Initial Commit

---
+++ SwiftLayout/SwiftLayout.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLayout.h
+//  SwiftLayout
+//
+//  Created by Nick Rudden on 11/30/19.
+//  Copyright © 2019 Nick Rudden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLayout.
+FOUNDATION_EXPORT double SwiftLayoutVersionNumber;
+
+//! Project version string for SwiftLayout.
+FOUNDATION_EXPORT const unsigned char SwiftLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLayout/PublicHeader.h>
+
+
